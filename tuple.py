7a2d65815af2966e1b89a5e4be121fff0a750307
SAYAN501 <sg1734164@gmail.com>
2026-01-27 07:29:56 -0800

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,28 @@
+'''
+a=(1,2,3)
+b=[1,2,3]
+b[0]=10
+print(b)
+
+c[0]=12
+a=tuple(c)
+print(a)
+
+
+a=(24,)+a[1:]
+print(a[:])
+
+a=[2,24,10,12,13,5,4,34,9,3]
+
+print(a[0:])
+print (a[1:9:2])
+print(a[-3])
+'''       
+             
+
+
+name="pop"
+print(name[0])
+print (name[-1])
+
+print (name == name[::-1])
